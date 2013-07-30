324727b844f93d896b132871daa3eedc138fec66
itsrachelfish <itsrachelfish@gmail.com>
2013-07-30 00:02:13 -0600

    Added lots of comments, fixed randomPos

--- botspot.pl
+++ botspot.pl
@@ -37,14 +37,14 @@ sub randomPos
     my($pos) = @_;
     
     my $offset = {
-        'x' => int(rand()) + 1,
-        'y' => int(rand()) + 1
+        'x' => int(rand(2)) + 1,
+        'y' => int(rand(2)) + 1
     };
     
-    if (int(rand()) == 0) {
+    if(int(rand(2))) {
         $offset->{x} *= -1;
     }
-    if (int(rand()) == 0) {
+    if(int(rand(2))) {
         $offset->{y} *= -1;
     }
 
@@ -57,31 +57,29 @@ sub parseChat
     my($hook, $args) = @_;
     my $user = $args->{MsgUser};
     my $message = $args->{Msg};
-    
-    print("$message\n");
-    
+        
     if($message =~ m/^Arrived at ([0-9]+), ([0-9]+)$/)
     {
         my $arrived = {x => $1, y => $2};
-    
-        print(Dumper($botspot->{pos}));
-        print(Dumper($arrived));
-        
-        my $distance = distance($botspot->{pos}, $arrived);
-        print(Dumper($distance));
     
         if($user == $botspot->{priest})
         {
             if($step->{priest} == 0)
             {
-                $botspot->{warpPos} = {x => $arrived->{x}, y =>$arrived->{y}};
-                
+                my $distance = distance($botspot->{pos}, $arrived);
+            
+                # The server moves us to the nearest available cell around the spammer
                 if($distance < 2) 
                 {
+                    # Move off the current cell before casting warp
                     my $random = randomPos($arrived);
                     Commands::run("pm '$botspot->{priest}' exec move $random->{x} $random->{y}");
+
+                    $botspot->{warpPos} = {x => $arrived->{x}, y =>$arrived->{y}};
                     $step->{priest} += 1
                 }
+                
+                # The spammer is surrounded by people and can't be warped easily :(
                 else
                 {
                     print("We can't dunk this man. He is undunkable.\n");
@@ -89,12 +87,14 @@ sub parseChat
             } 
             elsif($step->{priest} == 1)
             {
-                my $pos = calcPosition($char);
-                if($pos->{x} == $botspot->{warpPos}->{x} and $pos->{y} == $botspot->{warpPos}->{y})
+                # We tried to move randomly, but did we actually move off the warp position?
+                if($arrived->{x} == $botspot->{warpPos}->{x} and $arrived->{y} == $botspot->{warpPos}->{y})
                 {
                     my $random = randomPos($arrived);
                     Commands::run("pm '$botspot->{priest}' exec move $random->{x} $random->{y}");
-                 }
+                }
+                
+                # If so, cast warp!
                 else
                 {
                     Commands::run("pm '$botspot->{priest}' exec sl 27 $botspot->{warpPos}->{x} $botspot->{warpPos}->{y}");
@@ -126,9 +126,13 @@ sub start
         my $pos = calcPosition($player);
         $botspot->{target} = $player;
         $botspot->{pos} = $pos;
-        
+      
         print("Player '$player->{name}' matched at $pos->{x}, $pos->{y}\n");
+      
+        # Reset steps in case this isn't the first dunk
+        $step = {'priest' => 0, 'wizard' => 0, 'knight' => 0};        
         
+        # Tell our priest to walk on top of the spammer
         Commands::run("pm '$botspot->{priest}' exec move $pos->{x} $pos->{y}");
     }
     else
